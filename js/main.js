902688d2941bb5111dc640dd577a45e498fac3fc
Annie Huang <annie.hn@gmail.com>
2025-02-01 18:36:45 +1100

    Remove all the btn-selected and then add it to the button element we defined early.

--- js/main.js
+++ js/main.js
@@ -15,13 +15,23 @@ const handleScroll = () => {
 const handleColorButtonClick = (event) => {
   let button;
 
+  // exact element the mouse is clicked.
   // console.log(event.target);
+  // The parent element of the current element, If mouse is cilcked on one element (img within a button) in the list of element (buttons), then the parent (parent div to wrap the buttons) of the will be return.
+  // console.log(event.currentTarget);
+
   console.log(event.target.tagName); // it can print IMG, BUTTON, DIV, depend on where you click
 
   if (event.target.tagName === 'IMG') {
     button = event.target.closest('button');
   } else if (event.target.tagName === 'BUTTON') {
     button = event.target;
+  }
+
+  if (button) {
+    const buttons = event.currentTarget.querySelectorAll('button');
+    buttons.forEach((btn) => btn.classList.remove('btn-selected'));
+    button.classList.add('btn-selected');
   }
 };
 
