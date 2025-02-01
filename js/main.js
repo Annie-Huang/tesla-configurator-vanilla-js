3c038301eee1ddb6ce4d520e21f00b26ecf0eac1
Annie Huang <annie.hn@gmail.com>
2025-02-01 17:24:56 +1100

    Add addEventListener for scroll to call handleScroll function

--- js/main.js
+++ js/main.js
@@ -6,3 +6,8 @@ const handleScroll = () => {
   topBar.classList.toggle('visible-bar', atTop);
   topBar.classList.toggle('hidden-bar', !atTop);
 };
+
+// Event Listeners
+// requestAnimationFrame will increase performance. It's used to schedule the function to run at an optimal time for smooth visual updates
+// Without this, handlescroll would just be called hundreds of times or could be called hundreds of times per second and could potentially lead to Performance issues
+window.addEventListener('scroll', () => requestAnimationFrame(handleScroll));
